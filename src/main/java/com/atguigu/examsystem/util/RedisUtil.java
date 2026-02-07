f6ef8eafa4b8b543ac03962b754e76ae4564fb9b
kangkang <pjackk@163.com>
2026-02-07 10:46:36 +0800

    1、RedisUtil添加static方法

--- src/main/java/com/atguigu/examsystem/util/RedisUtil.java
+++ src/main/java/com/atguigu/examsystem/util/RedisUtil.java
@@ -3,10 +3,11 @@ package com.atguigu.examsystem.util;
 import cn.hutool.core.collection.CollUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Component;
 
-import java.time.Duration;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
@@ -18,18 +19,21 @@ public class RedisUtil {
     private static RedisTemplate<String, Object> redisTemplate;
 
     /* ============================= String类型 start ============================= */
+
     /**
      * 设置key-value
+     *
      * @param key
      * @param value
      */
     public static void set(String key, Object value) {
         // opsForValue是对字符串类型操作
-        redisTemplate.opsForValue().set(key,value);
+        redisTemplate.opsForValue().set(key, value);
     }
 
     /**
      * 设置k-v 过期时间second秒
+     *
      * @param key
      * @param value
      * @param second
@@ -40,6 +44,7 @@ public class RedisUtil {
 
     /**
      * 获取key对应的value值
+     *
      * @param key
      * @return
      */
@@ -49,6 +54,7 @@ public class RedisUtil {
 
     /**
      * 删除key缓存
+     *
      * @param key
      */
     public static void delete(String key) {
@@ -57,6 +63,7 @@ public class RedisUtil {
 
     /**
      * 批量删除keyList缓存
+     *
      * @param keyList
      */
     public static void deleteBatch(Collection<String> keyList) {
@@ -65,6 +72,7 @@ public class RedisUtil {
 
     /**
      * 设置key的过期时间
+     *
      * @param key
      * @param second
      */
@@ -74,6 +82,7 @@ public class RedisUtil {
 
     /**
      * 是否存在key
+     *
      * @param key
      * @return
      */
@@ -83,6 +92,7 @@ public class RedisUtil {
 
     /**
      * 根据pattern获取所有匹配的key
+     *
      * @param pattern
      * @return
      */
@@ -92,12 +102,13 @@ public class RedisUtil {
 
     /**
      * 根据pattern批量删除匹配到的key
+     *
      * @param pattern
      */
     public static void deleteByPattern(String pattern) {
         Set<String> keys = redisTemplate.keys(pattern);
         if (!CollUtil.isEmpty(keys)) {
-                deleteBatch(keys);
+            deleteBatch(keys);
         }
     }
 
@@ -107,6 +118,7 @@ public class RedisUtil {
 
     /**
      * 设置hash缓存
+     *
      * @param key
      * @param hashKey
      * @param value
@@ -117,6 +129,7 @@ public class RedisUtil {
 
     /**
      * 获取hash值
+     *
      * @param key
      * @param hashKey
      * @return
@@ -127,6 +140,7 @@ public class RedisUtil {
 
     /**
      * 删除hash值
+     *
      * @param key
      * @param hashKey
      * @return
@@ -137,6 +151,7 @@ public class RedisUtil {
 
     /**
      * 设置hash值
+     *
      * @param key
      * @param map
      */
@@ -146,6 +161,7 @@ public class RedisUtil {
 
     /**
      * 根据key获取到<field,value>集合
+     *
      * @param key
      * @return
      */
@@ -155,6 +171,7 @@ public class RedisUtil {
 
     /**
      * 判断是否存在key中是否存在hashKey字段
+     *
      * @param key
      * @param hashKey
      * @return
@@ -165,4 +182,236 @@ public class RedisUtil {
 
     /* ============================= Hash类型 end ============================= */
 
+
+    /* ============================= List类型 start ============================ */
+
+    /**
+     * 将value值放入key键中
+     *
+     * @param key
+     * @param value
+     * @return
+     */
+    public static long lPush(String key, Object value) {
+        return redisTemplate.opsForList().rightPush(key, value);
+    }
+
+    /**
+     * 将value值放入缓存，超过timeout秒则执行超时
+     *
+     * @param key
+     * @param value
+     * @param timeout 超时时间
+     * @return
+     */
+    public static long lPush(String key, Object value, long timeout) {
+        Long count = redisTemplate.opsForList().rightPush(key, value);
+        expire(key, timeout);
+        return count;
+    }
+
+    /**
+     * 将values放入缓存
+     *
+     * @param key
+     * @param values
+     * @return
+     */
+    public static long lPushAll(String key, Object... values) {
+        return redisTemplate.opsForList().rightPushAll(key, values);
+    }
+
+    /**
+     * 将values放入缓存，超过timeout秒执行超时
+     *
+     * @param key
+     * @param timeout 超时时间
+     * @param values
+     * @return
+     */
+    public static long lPushAll(String key, long timeout, Object... values) {
+        Long count = redisTemplate.opsForList().rightPushAll(key, values);
+        expire(key, timeout);
+        return count;
+    }
+
+    /**
+     * 查出key索引从start到end的值，包含start和end
+     *
+     * @param key
+     * @param start
+     * @param end
+     * @return
+     */
+    public static List<Object> lRange(String key, long start, long end) {
+        return redisTemplate.opsForList().range(key, start, end);
+    }
+
+    /**
+     * 获取key的值的长度
+     *
+     * @param key
+     * @return
+     */
+    public static long lSize(String key) {
+        return redisTemplate.opsForList().size(key);
+    }
+
+    /**
+     * 获取指定index位置的key的值
+     *
+     * @param key
+     * @param index
+     * @return
+     */
+    public static Object lIndex(String key, long index) {
+        return redisTemplate.opsForList().index(key, index);
+    }
+
+    /**
+     * 移除指定数量的key值为value的值
+     *
+     * @param key
+     * @param count
+     * @param value
+     * @return 返回真正移除value值的数量
+     */
+    public static Long lRemove(String key, long count, Object value) {
+        return redisTemplate.opsForList().remove(key, count, value);
+    }
+
+    /* ============================= List类型 end ============================ */
+
+    /* ============================= Zset类型 start ============================ */
+
+    /**
+     * 向有序集合中添加元素，如果元素已存在，则更新分数
+     *
+     * @param key
+     * @param value
+     * @param score
+     * @return
+     */
+    public Boolean zAdd(String key, Object value, double score) {
+        return redisTemplate.opsForZSet().add(key, value, score);
+    }
+
+    /**
+     * 增添key键值为value的score分数，增加delta，delta值可以是负数
+     *
+     * @param key
+     * @param value
+     * @param delta
+     * @return 修改后的score数据
+     */
+    public Double zAdd(String key, Object value, long delta) {
+        return redisTemplate.opsForZSet().incrementScore(key, value, delta);
+    }
+
+    /**
+     * 返回key的值为value的分数
+     *
+     * @param key
+     * @param value
+     * @return score分数
+     */
+    public Double zScore(String key, Object value) {
+        return redisTemplate.opsForZSet().score(key, value);
+    }
+
+    /**
+     * 返回key中value值的数量
+     *
+     * @param key
+     * @return
+     */
+    public Long zSize(String key) {
+        return redisTemplate.opsForZSet().size(key);
+    }
+
+    /**
+     * 根据score最小值和最大值，获取key的值的集合
+     *
+     * @param key
+     * @param min
+     * @param max
+     * @return key的值的集合
+     */
+    public Set<Object> zRangeByScore(String key, Double min, Double max) {
+        return redisTemplate.opsForZSet().rangeByScore(key, min, max);
+    }
+
+    /**
+     * 返回value值的索引位置
+     *
+     * @param key
+     * @param value
+     * @return
+     */
+    public Long zRank(String key, Object value) {
+        return redisTemplate.opsForZSet().rank(key, value);
+    }
+
+
+    /**
+     * 获取key的值的集合，按score从低到高排序（默认排序按score值从低到高）
+     *
+     * @param key
+     * @param start
+     * @param end
+     * @return
+     */
+    public Set<Object> zRange(String key, long start, long end) {
+        return redisTemplate.opsForZSet().range(key, start, end);
+    }
+
+
+    /**
+     * 获取key的值和score分数的集合，按score从低到高排序（默认排序按score值从低到高）
+     *
+     * @param key
+     * @param start
+     * @param end
+     * @return
+     */
+    public Set<ZSetOperations.TypedTuple<Object>> zRangeWithScores(String key, long start, long end) {
+        return redisTemplate.opsForZSet().rangeWithScores(key, start, end);
+    }
+
+    /**
+     * 获取key的值的集合，按score从高到低排序（倒序排序）
+     *
+     * @param key
+     * @param start
+     * @param end
+     * @return
+     */
+    public Set<Object> zReverseRange(String key, long start, long end) {
+        return redisTemplate.opsForZSet().reverseRange(key, start, end);
+    }
+
+
+    /**
+     * 获取key的值的集合，按score从高到低排序（倒序排序）
+     *
+     * @param key
+     * @param start
+     * @param end
+     * @return
+     */
+    public Set<ZSetOperations.TypedTuple<Object>> zReverseRangeWithScores(String key, long start, long end) {
+        return redisTemplate.opsForZSet().reverseRangeWithScores(key, start, end);
+    }
+
+    /**
+     * 删除key的value集合值，返回删除数量
+     * @param key
+     * @param values
+     * @return
+     */
+    public long zRemove(String key, Object ...values) {
+        return redisTemplate.opsForZSet().remove(key, values);
+    }
+
+    /* ============================= Zset类型 end ============================ */
 }
